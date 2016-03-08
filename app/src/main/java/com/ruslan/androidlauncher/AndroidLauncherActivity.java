58e9e884255c6ebe77a9e3942ed1ca01c303057c
ruslan <ruslan.gmz@gmail.com>
2016-03-08 16:40:12 +0300

    Initial commit

---
+++ app/src/main/java/com/ruslan/androidlauncher/AndroidLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.ruslan.androidlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+
+        return AndroidLauncherFragment.newInstance();
+    }
+}
